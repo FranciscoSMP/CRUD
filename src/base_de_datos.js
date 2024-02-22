692481a677b1397ad11bc7e43eddbdc2be548ecb
FranciscoSMP <fmoralesp2001@gmail.com>
2024-02-22 00:32:34 -0600

    22/02/2024

--- src/base_de_datos.js
+++ src/base_de_datos.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const { promisify } = require('util');
 
 const {base_de_datos} = require('./keys');
 
@@ -6,9 +7,22 @@ const pool = mysql.createPool(base_de_datos);
 
 pool.getConnection((err, connection) => {
     if (err.code === 'PROTOCOL_CONNECTION_LOST'){
-        console.error('DATABASE CONNECTION WAS CLOSED');
+        console.error('LA CONEXIÓN DE LA BASE DE DATOS SE CERRÓ');
     }
     if (err.code === 'ER_CON_COUNT_ERROR'){
-        console.error('DATABASE HAS TO MANY CONNECTIONS');
+        console.error('LA BASE DE DATOS TIENE MUCHAS CONEXIONES');
     }
+    if (err.code === 'ECONNREFUSED'){
+        console.error('CONEXIÓN DE LA BASE DE DATOS RECHAZADA');
+    }
+
+    if (connection) connection.release();
+    console.log('La Base de Datos está Conectada');
+    return;
 });
+
+// Promisify Poo Querys.
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
--- src/routes/notas.js
+++ src/routes/notas.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const pool = require('../base_de_datos');
+
+router.get('/add', (req, res) => {
+    res.render('notas/add');
+});
+
 module.exports = router;
