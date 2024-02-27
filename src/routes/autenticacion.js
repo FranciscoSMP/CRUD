a3b873fe206e04b96227d27c5a44492d6e3e2a5c
FranciscoSMP <fmoralesp2001@gmail.com>
2024-02-26 23:11:49 -0600

    26/02/2024

--- src/routes/autenticacion.js
+++ src/routes/autenticacion.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/registro', (req, res) => {
+    res.render('')
+});
+
+router.post('/registro', (req, res) => {
+
+});
+
 module.exports = router;
